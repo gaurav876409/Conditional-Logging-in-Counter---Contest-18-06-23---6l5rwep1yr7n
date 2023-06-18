b0337fcbe79c8ca842e3f253e9e0609d8c9ef117
gaurav876409 <gaurav876409@gmail.com>
2023-06-18 13:30:39 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,26 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component{
-  
+  constructor(props){
+    super(props);
+    this.state = { count: 0};
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    const isCountEven = nextState.count %2 === 0;
+
+    if(isCountEven){
+      return true;
+    }
+    return false;
+  }
   handleClick(){
     this.setState({count: this.state.count + 1})
   }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this)
-  }
+  // constructor(props){
+  //   super(props)
+  //   this.state = {count:0}
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
 
 
   render(){
