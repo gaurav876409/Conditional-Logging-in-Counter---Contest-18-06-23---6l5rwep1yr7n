097c30b6d8503a5a1772b04cf5ec65b575d3954d
gaurav876409 <gaurav876409@gmail.com>
2023-06-18 13:58:47 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,9 @@ class App extends React.Component{
     }
     return false;
   }
-  handleClick(){
-    this.setState({count: this.state.count + 1})
-  }
+  handleClick = () => {
+    this.setState({count: this.state.count + 1});
+  };
   // constructor(props){
   //   super(props)
   //   this.state = {count:0}
@@ -31,8 +31,7 @@ class App extends React.Component{
         <span id="count">{this.state.count}</span>
         <button id="incr-btn" onClick={this.handleClick}>Increment</button>
       </div>
-    )
-
+    );
   }
 }
 
